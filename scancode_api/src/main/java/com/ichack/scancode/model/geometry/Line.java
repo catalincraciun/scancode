d31a9b943c79ece8f2963ff8ff53b3ffa3ea7ed5
Calin Farcas <cf316@ic.ac.uk>
2017-08-26 11:09:24 +0100

    updated Line

--- scancode_api/src/main/java/com/ichack/scancode/model/geometry/Line.java
+++ scancode_api/src/main/java/com/ichack/scancode/model/geometry/Line.java
@@ -7,8 +7,8 @@ public class Line {
 
   private static final double EPSILON = 0.001;
 
-  private final Point p1;
-  private final Point p2;
+  private final Point<Double> p1;
+  private final Point<Double> p2;
 
   private final double a;
   private final double b;
@@ -22,20 +22,20 @@ public class Line {
    * @param p1 One end of the line
    * @param p2 The other end of the line
    */
-  public Line(Point p1, Point p2) {
+  public Line(Point<Double> p1, Point<Double> p2) {
     this.p1 = p1;
     this.p2 = p2;
 
-    if (p2.getY().doubleValue() - p1.getY().doubleValue() >= EPSILON ||
-        p1.getY().doubleValue() - p2.getY().doubleValue() >= EPSILON) {
+    if (p2.getY() - p1.getY() >= EPSILON ||
+        p1.getY() - p2.getY() >= EPSILON) {
       this.a = 1;
-      this.b = (p2.getX().doubleValue() - p1.getX().doubleValue()) / (p1.getY().doubleValue() - p2.getY().doubleValue());
-      this.c = -(a * p1.getX().doubleValue() + b * p1.getY().doubleValue());
+      this.b = (p2.getX() - p1.getX()) / (p1.getY() - p2.getY());
+      this.c = -(a * p1.getX() + b * p1.getY());
       isVertical = false;
     } else {
       this.a = 0;
       this.b = 1;
-      this.c = -p1.getY().doubleValue();
+      this.c = -p1.getY();
       isVertical = true;
     }
   }
@@ -44,15 +44,15 @@ public class Line {
    * Calculates a translated point on this line, by using the line as a unit
    * It will return the middle of the line for scale = 1 / 2
    *
-   * @implNote This will go from p1 to p2, sense wise
+   * @implNote This will go from p1 to p2, direction wise
    *
    * @param scale The number of units used for translation
    * @return The translated Point, always located on this line
    */
   public Point getTranslation(double scale) {
-    return new Point(
-        (p2.getX().doubleValue() - p1.getX().doubleValue()) * scale + p1.getX().doubleValue(),
-        (p2.getY().doubleValue() - p1.getY().doubleValue()) * scale + p1.getY().doubleValue());
+    return new Point<>(
+        (p2.getX() - p1.getX()) * scale + p1.getX(),
+        (p2.getY() - p1.getY()) * scale + p1.getY());
   }
 
   /**
@@ -64,13 +64,13 @@ public class Line {
   public Point getIntersection(Line other) {
     if (!isVertical && !other.isVertical) {
       double y = (c - other.c) / (other.b - b);
-      return new Point(-c - y * b, y);
+      return new Point<>(-c - y * b, y);
     } else if (isVertical) {
       double y = c;
-      return new Point((-other.c - other.b * y) / other.a, y);
+      return new Point<>((-other.c - other.b * y) / other.a, y);
     } else {
       double y = other.c;
-      return new Point((-c - b * y) / a, y);
+      return new Point<>((-c - b * y) / a, y);
     }
   }
 
