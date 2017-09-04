ab35497349d8f5c1e92c4dd3f6c6a10af54eeaa6
Calin Farcas <cf316@ic.ac.uk>
2017-09-04 15:24:08 +0100

    added StorageGuard interface methods

--- scancode_api/src/main/java/com/ichack/scancode/model/storage/DbStorageGuard.java
+++ scancode_api/src/main/java/com/ichack/scancode/model/storage/DbStorageGuard.java
@@ -2,19 +2,18 @@ package com.ichack.scancode.model.storage;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
-import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class DbStorageGuard implements StorageGuard {
 
   private static final String DATABASENAME = "DatabaseCodeStorage";
   private static final String HOST = "localhost";
-  private static final Integer PORT = 27017;
+  private static final int PORT = 27017;
   private static final String COLLECTION = "dataCollection";
 
   private MongoClient mongo;
@@ -50,5 +49,35 @@ public class DbStorageGuard implements StorageGuard {
       db.createCollection(COLLECTION);
       collection = db.getCollection(COLLECTION);
     }
+  }
+
+  @Override
+  public boolean containsData(long code) {
+    return collection.find(Filters.eq("code", code)).iterator().hasNext();
+  }
+
+  @Override
+  public void add(long code, String data) {
+    //simulate map behaviour, delete entry if it already exists
+    if (containsData(code)) {
+      collection.deleteOne(collection.find(Filters.eq("code", code))
+          .iterator().next());
+    }
+
+    Document toAdd = new Document("code", code)
+        .append("data", data);
+    collection.insertOne(toAdd);
+  }
+
+  @Override
+  public String getData(long code) {
+    if (!containsData(code)) {
+      throw new NoSuchElementException("There is no data for that code");
+    }
+
+    Document entry = collection.find(Filters.eq("code", code))
+        .iterator().next();
+
+    return (String) entry.get("data");
   }
 }
